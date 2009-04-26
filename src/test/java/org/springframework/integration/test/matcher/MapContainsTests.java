6167ea13d6b2c0e2c2e9b43f898b2f249104736e
alpe <info@alexanderpeters.de>
2009-04-26 22:17:35 +0200

    added testcases

--- src/test/java/org/springframework/integration/test/matcher/MapContainsTests.java
+++ src/test/java/org/springframework/integration/test/matcher/MapContainsTests.java
@@ -1,9 +1,16 @@
 package org.springframework.integration.test.matcher;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.springframework.integration.test.matcher.MapContains.hasAllEntries;
+import static org.springframework.integration.test.matcher.MapContains.hasEntry;
+import static org.springframework.integration.test.matcher.MapContains.hasKey;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -33,15 +40,65 @@ public class MapContainsTests {
 		map.put(OTHER_KEY, OTHER_VALUE);
 	}
 
-	// see HeaderMatcherTests
-
 	@Test
 	public void hasKey_validKey_matching() throws Exception {
-		assertThat(map, MapContains.hasKey(ANY_KEY));
+		assertThat(map, hasKey(ANY_KEY));
+
 	}
 
 	@Test
 	public void hasKey_unknownKey_notMatching() throws Exception {
-		assertThat(map, not(MapContains.hasKey(UNKNOWN_KEY)));
+		assertThat(map, not(hasKey(UNKNOWN_KEY)));
+	}
+
+	@Test
+	public void hasEntry_withValidKeyValue_matches() throws Exception {
+		assertThat(map, hasEntry(ANY_KEY, ANY_VALUE));
+		assertThat(map, hasEntry(OTHER_KEY, OTHER_VALUE));
+	}
+
+	@Test
+	public void hasEntry_withUnknownKey_notMatching() throws Exception {
+		assertThat(map, not(hasEntry("test.unknown", ANY_VALUE)));
+	}
+
+	@Test
+	public void hasEntry_withValidKeyAndMatcherValue_matches() throws Exception {
+		assertThat(map, hasEntry(ANY_KEY, is(String.class)));
+		assertThat(map, hasEntry(ANY_KEY, notNullValue()));
+		assertThat(map, hasEntry(ANY_KEY, is(ANY_VALUE)));
+	}
+
+	@Test
+	public void hasEntry_withValidKeyAndMatcherValue_notMatching() throws Exception {
+		assertThat(map, not(hasEntry(ANY_KEY, is(Integer.class))));
+	}
+
+	@Test
+	public void hasEntry_withTypedValueMap_matches() throws Exception {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("a", "b");
+		map.put("c", "d");
+		assertThat(map, hasEntry("a", "b"));
+		assertThat(map, not(hasEntry(ANY_KEY, is("a"))));
+		assertThat(map, hasAllEntries(map));
+	}
+
+	@Test
+	public void hasAllEntries_withValidKeyValueOrMatcherValue_matches() throws Exception {
+		Map<String, Object> expectedInHeaderMap = new HashMap<String, Object>();
+		expectedInHeaderMap.put(ANY_KEY, ANY_VALUE);
+		expectedInHeaderMap.put(OTHER_KEY, is(OTHER_VALUE));
+		assertThat(map, hasAllEntries(expectedInHeaderMap));
+	}
+
+	@Test
+	public void hasAllEntries_withInvalidValidKeyValueOrMatcherValue_notMatching() throws Exception {
+		Map<String, Object> expectedInHeaderMap = new HashMap<String, Object>();
+		expectedInHeaderMap.put(ANY_KEY, ANY_VALUE); // valid
+		expectedInHeaderMap.put(UNKNOWN_KEY, not(nullValue())); // fails
+		assertThat(map, not(hasAllEntries(expectedInHeaderMap)));
+		expectedInHeaderMap.remove(UNKNOWN_KEY);
+		expectedInHeaderMap.put(OTHER_KEY, ANY_VALUE); // fails
 	}
 }
